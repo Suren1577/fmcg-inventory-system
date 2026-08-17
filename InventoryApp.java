1d1f4e0105cd85a7bb75899162dbee6c6929cff0
Suren1577 <surenrare@gmail.com>
2026-08-17 16:24:47 +0000

    Add inventory management feature

---
+++ InventoryApp.java
@@ -0,0 +1,11 @@
+package com.fmcg;
+
+public class InventoryApp {
+
+    public static void main(String[] args) {
+       System.out.println("FMCG Inventory System");
+System.out.println("Product Management Feature");
+System.out.println("Checking product inventory...");
+System.out.println("Inventory Management Feature");
+    }
+}
