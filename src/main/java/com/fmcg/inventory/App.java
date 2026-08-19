b903c904934eb59c139302d3bc791f77c5f1973d
Suren1577 <surenrare@gmail.com>
2026-08-19 14:57:02 +0000

    Add Maven build and Jenkins pipeline

---
+++ src/main/java/com/fmcg/inventory/App.java
@@ -0,0 +1,12 @@
+package com.fmcg.inventory;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("FMCG Inventory Management System");
+
+        System.out.println("Product: Rice");
+        System.out.println("Stock: 100");
+    }
+}
